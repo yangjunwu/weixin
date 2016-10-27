bdbb5591eaf3999a17cc7303d4b6f8aabbdaf977
杨俊武 <yangjw@origimed.com>
2016-10-27 16:13:47 +0800

    wx

---
+++ notice.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title></title>
+	<link rel="stylesheet" href="css/all.css?rev=@@hash">
+	<link rel="stylesheet" href="css/notice_.css?rev=@@hash">
+</head>
+<body>
+	<header>
+		<a href="javascript:;" class="pull_left"></a>
+		<span>待办通知</span>
+		<a href="./search.php" class="pull_right"><i class="weui_icon_search"></i></a>
+	</header>
+	<div class="content-padded" id="cp">
+		
+		
+		
+
+	</div>
+	<div class="weui-infinite-scroll">
+		<div class="infinite-preloader"></div>
+		正在加载...
+	</div>
+
+
+
+	<script src="js/all.js?rev=@@hash"></script>
+	<script src="js/common.js?rev=@@hash"></script>
+	<script src="js/list.js?rev=@@hash"></script>
+
+</body>
+</html>
